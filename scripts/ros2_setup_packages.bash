4cec2f026bd06c7d2684c15a8bbedb52e52c48ee
Aaron Spiteri <azzmosphere@gmail.com>
2025-10-01 17:49:27 +1000

    removed unneeded code

--- scripts/ros2_setup_packages.bash
+++ scripts/ros2_setup_packages.bash
@@ -17,7 +17,6 @@ aptinstall python3-picamera2
 U=${USER}
 sudo adduser $U video
 
-
 aptinstall python3-colcon-meson
 mdkir -p ${PPWD}/src
 
@@ -40,7 +39,6 @@ fi
 cd ${PPWD}/src
 
 echo "INFO: setting up Micro-XRCE-DDS-Agent"
-# sudo apt install -y ros-$ROS_DISTRO-rmw-fastrtps-cpp
 source /opt/ros/ROS_DISTRO/setup.bash
 if [[ ! -d  ${PPWD}/src/Micro-XRCE-DDS-Agent ]]; then
     cd ${PPWD}/src
@@ -53,43 +51,5 @@ fi
 
 rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO --skip-keys=libcamera
 colcon build --event-handlers=console_direct+
-
-
-
-
-# CAMERA_ROS_VERSION='0.5.0'
-# XRCE_DSS_VERSION='v3.0.1'
-
-# function git_clone_pull() {
-#     cd ${PPWD}/src
-#     R=${1}
-#     D=${2}
-#     V=${3}
-#     M=${4}
-
-#     if [[ ! -e ${D} ]]; then
-#         git clone ${R} || fail "unable to clone camera ros"
-#     fi
-
-#     cd ${PPWD}/src/${D}
-#     git checkout ${M}
-#     git branch -d ${V}
-#     git fetch && git pull origin ${M}
-#     git checkout -b ${V} tags/${V}
-
-# }
-
-
-
-# if [[ ! -e ${PPWD}/src ]]; then
-#     mkdir -p ${PPWD}/src
-# fi
-
-# cd ${PPWD}/src
-# echo "INFO: entered ${PPWD}/src"
-# echo "INFO: refreshing camera_ros:${CAMERA_ROS_VERSION}"
-# git_clone_pull 'https://github.com/christianrauch/camera_ros.git' camera_ros ${CAMERA_ROS_VERSION} main
-
-
 
 exit 0
