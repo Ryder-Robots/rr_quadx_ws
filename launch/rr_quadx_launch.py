13a4c271883d653ee9618ce7856f0a971e601ab1
Aaron Spiteri <azzmosphere@gmail.com>
2025-10-01 16:36:04 +1000

    committing initial launch file

--- launch/rr_quadx_launch.py
+++ launch/rr_quadx_launch.py
@@ -1,22 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+from ament_index_python.packages import get_package_share_directory
 
-def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='camera_ros',
-            executable='camera_node',
-            name='camera',
-            output='screen',
-            parameters=[{
-                'camera': "imx708",          # Camera index or name
-                'role': 'viewfinder',        # Stream role: raw, still, video, viewfinder
-                'format': 'SBGGR10_1X10',    # Pixel format (auto by default)
-                'width': 640,                # Image width
-                'height': 480,               # Image height
-                'frame_rate': 30,            # Frame rate (fps)
-                'camera_info_url': '/home/aaron/rr_quadx_ws/etc/imx708.yaml',       # Path/URL to calibration YAML
-                'orientation': 180             # Camera orientation in degrees (0, 90, 180, 270)
-            }]
-        )
-    ])
