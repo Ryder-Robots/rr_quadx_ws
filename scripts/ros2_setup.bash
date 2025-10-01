b3387c95d2a103864d6e6b6497427436c48d280b
Aaron Spiteri <azzmosphere@gmail.com>
2025-10-01 10:39:15 +1000

    fixed typo

--- scripts/ros2_setup.bash
+++ scripts/ros2_setup.bash
@@ -65,7 +65,7 @@ sudo add-apt-repository universe | fail "could not add ros repositories"
 echo "INFO: installing ros2-apt source packages"
 aptinstall curl
 export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
-$ curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
 sudo dpkg -i /tmp/ros2-apt-source.deb
 
 echo "INFO: installing ROS2 development tools"
